0b2ea86754311df0c34e5f98253136492960c158
Sujith <p.sujithk@gmail.com>
2023-11-22 17:03:50 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './Components/Navbar';
+import Navbar from './Components/Navbar/Navbar';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 
