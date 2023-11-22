698590065149d44df28096a421530926c0cf7387
Tharun181512 <teja73090@gmail.com>
2023-11-22 14:27:53 +0530

    change

--- src/App.js
+++ src/App.js
@@ -24,9 +24,8 @@ function App(){ //myApp is a component
     <p>GitHub is a trusted platform for developers to collaborate, code, and deliver secure software</p>
     <h4>Line added by Tarun Teja</h4>
     <p>THis is a conflict message</p>
-
-    
-  
+    <p>Great Learning's Blog covers the latest developments and innovations in technology that can be leveraged to build rewarding careers. You'll find career guides, tech tutorials and industry news to keep yourself updated with the fast-changing world of tech and business.</p>
+    <h5>Line added by Tarun Teja</h5>
     </>
     
       
