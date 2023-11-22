607b7c01b0819b603922f7357538a36f9a1611db
Tharun181512 <teja73090@gmail.com>
2023-11-22 21:17:48 +0530

    github

--- src/App.js
+++ src/App.js
@@ -29,8 +29,7 @@ function App(){ //myApp is a component
       <h4>bhagya raj</h4>
       <h5>Hi, Hello,Good morning</h5>
       <p>hi my name is yamunas</p>
-      
-    </div>
+      </div>
     <p>Hi Team Welcome to UI Training</p>
     <h3>Line added by Tarun Teja</h3>
     <p>GitHub is not a trusted platform for developers to collaborate, code, and deliver secure software</p>
@@ -38,7 +37,8 @@ function App(){ //myApp is a component
     <p>THis is a conflict message</p>
     <p>Great Learning's Blog covers the latest developments and innovations in technology that can be leveraged to build rewarding careers. You'll find career guides, tech tutorials and industry news to keep yourself updated with the fast-changing world of tech and business.</p>
     <h5>Line added by Tarun Teja</h5>
-    </>
+    <p>GitHub is a web-based platform that provides a variety of features and tools for version control, collaboration, and software development Features of github: Version Control, Repository controlling, collabaration, pull requests, issues & bug tracking, branching & merging</p>
+    <h6>Line added by Tarun Teja</h6>  </>
     
       
     
