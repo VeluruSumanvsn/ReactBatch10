cf86479161039d267dabb5cf688a952559e0cc93
Bhagyaraj1512 <bhagyakandipati61@gmail.com>
2023-11-23 19:18:57 +0000

    Added anchor for testing the conflicts

--- src/Components/Practice/Practice.js
+++ src/Components/Practice/Practice.js
@@ -6,7 +6,12 @@ function Practice(){
         <div>
             <h2>UI Practice</h2>
             <p>Planning to become a React Developer</p>
-            
+            <div>
+                <p>
+                    React_Practise
+                </p>
+                <a href="#" onClick={()=>{alert("Hello I am UI dev")}}>Hello</a>
+            </div>
         </div>
     )
 };
