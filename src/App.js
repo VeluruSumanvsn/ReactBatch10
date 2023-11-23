991f88c2c69d0e4cffce86564b626841ae83fda2
sirishanavuru <sirishanavuru@gmail.com>
2023-11-23 10:57:59 +0530

    sireeshaNs

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
+import Practice from './Practice';
 
 function App(){ //myApp is a component
   return(
     <>
+    <Practice />
     <Navbar />
     <Header />
     <Footer />
+
 
     <hr/>
     <div>
