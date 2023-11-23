94d8f2991eb620712ce445f705d6be3143a75414
kdinesh007 <dineshkanaparthi.ui@gmail.com>
2023-11-23 12:42:10 +0530

    New Component added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Practice from './Components/Practice/Practice';
-
+import NewComp from './Components/NewComp/NewComp';
 function App(){ 
   return(
     <>
@@ -11,9 +11,10 @@ function App(){
     <Navbar />
     <Header />
     <Footer /> 
+    <NewComp />
 
 
-    <hr/>
+    <hr/> 
     <div>
       <div>Basic Example1fafsdfjdgfjqagjf</div>
       <h2>bfvdjasgfvb</h2>
@@ -28,13 +29,14 @@ function App(){
       <h5>React Developer</h5>
       <h2>UI Training</h2>
       <h1>Practice</h1>
-      <h4>KANAPARHI DINESH</h4>
+      
+      
       <p>Hello team good morning</p>
       <h2> adding to Git</h2>
       <h4>bhagya raj</h4>
       <h5>Hi, Hello,Good morning</h5>
       <p>hi my name is yamunas</p>
-      </div>
+      </div> 
     <p>Hi Team Welcome to UI Training</p>
     <h3>Line added by Tarun Teja</h3>
     <p>GitHub is not a trusted platform for developers to collaborate, code, and deliver secure software</p>
@@ -55,7 +57,7 @@ function App(){
     <p>Git is responsible for everything GitHub-related that happens locally on your computer. You can read documentation that reflects the GitHub product you're currently using. Accounts on GitHub allow you to organize and control access to code. You can find ways to contribute to open source projects on GitHub.com that are relevant to you.</p>
     <h11>Line added by Tarun Teja</h11>
     <p>Github for collabaration: Forks: users can fork a repository to their own copy. this allows them to make changes independently & propose those changes back to the original repository through a pull request. Repository owners can add collabarators giving them permission to push changes directly to the repository Pull requests facilitate code review, where contributors can comment on specific lines of code, discuss changes, and ensure that the code meets quality standards.</p>
-    <h12>Line added by Tarun Teja</h12>
+    <h12>Line added by Tarun Teja</h12> 
     </>
   )
 }
