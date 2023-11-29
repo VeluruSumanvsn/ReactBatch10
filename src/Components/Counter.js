0ac0cfe3171da434aef732a73de9160866596bd4
jyothi <jyothirmayeemannepalli1997@gmail.com>
2023-11-29 11:21:37 +0530

    counter added

---
+++ src/Components/Counter.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+// import styles from "./counter.css";
+
+function Counter(){
+    const[count,setCount]=useState(0);
+    const increase=()=>{
+        setCount(count + 1)
+    }
+    const decrease=()=>{
+        setCount(count -1)
+    }
+    const reset=()=>{
+        setCount(0)
+    }
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increase}>increase</button>
+            <button onClick={reset}>reset</button>
+            <button onClick={decrease}>decrease</button>
+           {/* <Counter.css /> */}
+        </div>
+    )
+}
+export default Counter;
