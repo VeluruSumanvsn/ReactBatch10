530b19c06b94ff9535cafa075468c165645e805f
Tharun181512 <teja73090@gmail.com>
2023-11-29 16:46:48 +0530

    Added Adress bar in Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,12 +2,13 @@ import React from 'react';
 import "./Header.css"
 import Banner from './Banner/Banner';
 import Loginform from './Loginform/Loginform';
-
+import Address from './Addressbar/Address';
 const Header = () => {
 return (
     <>
     <Banner />
     <Loginform />
+    <Address />
     </>
 )
 }
