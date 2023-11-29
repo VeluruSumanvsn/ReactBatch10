97a68c8048ee3ad84b7a877aa1a9451d8eb404a3
Bhagyaraj1512 <bhagyakandipati61@gmail.com>
2023-11-29 17:21:04 +0000

    added news letter to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,12 +2,14 @@ import React from 'react';
 import "./Header.css"
 import Banner from './Banner/Banner';
 import Loginform from './Loginform/Loginform';
+import Newsletter from './Newsletter/Newsletter';
 
 const Header = () => {
 return (
     <>
     <Banner />
     <Loginform />
+    <Newsletter />
     </>
 )
 }
---
+++ src/Components/Header/Newsletter/Newsletter.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Newsletter.css";
+
+const Newsletter=() =>{
+    return(
+        <div className="main-content">
+           <h1>News letter</h1>
+           <h2>Get Latest News on Branded Shoes</h2>
+           <div className="input-content">
+            <input type="email" placeholder="Your Email id"/>
+            <button>
+               Submit
+            </button>
+           </div>
+
+
+        </div>
+        
+
+
+
+
+    )
+
+
+}
+
+
+
+export default Newsletter
