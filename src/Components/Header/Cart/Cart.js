095c376fd12639f7ab9f0e37bfeb4ffb374a3b0c
Tharun181512 <teja73090@gmail.com>
2023-12-01 15:26:07 +0530

    Added Cart Component To Header

--- src/Components/Header/Cart/Cart.js
+++ src/Components/Header/Cart/Cart.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import "./Cart.css" 
+import shoe_picture from "../../../Assets/product1.jpg"
 
 const Cart = () => {
 return (
     <>
-    <div>
-      <h1>This is cart page</h1>
-    </div>
+     <div class="cart-container">
+      <div class="cart-products">
+       <div class="cart-products-image">
+       <img src={shoe_picture} alt="Nike Shoe"/>
+      </div>
+      <div class="product-details">
+        <h3>Product Name</h3>
+        <p>Price: 2000</p>
+        <p>Quantity: 1</p>
+      </div>
+      <button class="cart-remove-button">Remove</button>
+      </div>
+      </div>
     </>
-)
-}
+);
+};
 
 export default Cart;
