81b94871fa6253dda746ce6d4cfc761241ea5c80
Bhagyaraj1512 <bhagyakandipati61@gmail.com>
2023-12-02 23:50:37 +0000

    small changes added

--- src/Components/Header/Newsletter/Newsletter.js
+++ src/Components/Header/Newsletter/Newsletter.js
@@ -1,21 +1,19 @@
 import React from "react";
-import "./Newsletter.css";
+// import "./Newsletter.css";
 
-const Newsletter=() =>{
-    return(
-        <div className="main-content" id="newsletter">
-           <h1>Newsletter</h1>
-           <h2>Get Latest News on Branded Shoes</h2>
-           <div className="input-content">
-            <input type="email" placeholder="Your Email id"/>
-            <button>
-               Subscribe
-            </button>
-           </div>
+const Newsletter = () => {
+  return (
+        <div className="address-bar card">
+        <div className="address-bar-content card-body d-flex align-items-center flex-column justify-content-center flex-shrink-1">
+          <h1 className="card-title h-100">Newsletter</h1>
+          <h2 className="card-text">Get Latest News on Branded Shoes</h2>
+          <div className=" address-bar-input card-body d-flex justify-content-center flex-row">
+            <input type="email" placeholder="Your Email id" />
+            <button className="btn btn-primary">Subscribe</button>
+          </div>
+            </div>
         </div>
-    )
-}
-
-
+  );
+};
 
 export default Newsletter;
